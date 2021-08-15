9165e5a1d7cf892858ae687462644ad8a6d904de
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-08-15 14:43:53 +0700

    create panel and set viewportview

--- src/main/java/com/defalt/usermanagement/UserService.java
+++ src/main/java/com/defalt/usermanagement/UserService.java
@@ -1,28 +1,53 @@
 package com.defalt.usermanagement;
+
 import java.util.*;
+
 public class UserService {
+
     private static ArrayList<User> userList = null;
-    static{
+
+    static {
         userList = new ArrayList<>();
-        userList.add(new User("admin","password"));
-        userList.add(new User("user1","password"));
-        userList.add(new User("user2","password"));
-    }public static boolean addUser(User user){
-        if(userList.add(user)){
+        userList.add(new User("admin", "password"));
+        userList.add(new User("user1", "password"));
+        userList.add(new User("user2", "password"));
+    }
+
+    public static boolean addUser(User user) {
+        if (userList.add(user)) {
             return true;
-        }return false;
-    }public static boolean delUser(User user){
-        if(userList.remove(user)){
+        }
+        return false;
+    }
+
+    public static boolean delUser(User user) {
+        if (userList.remove(user)) {
             return true;
-        }return false;
-    }public static boolean delUser(int index){
-        if(userList.remove(index)!=null){
+        }
+        return false;
+    }
+
+    public static boolean delUser(int index) {
+        if (userList.remove(index) != null) {
             return true;
-        }return false;
-    }public static ArrayList<User> getUser(){
+        }
+        return false;
+    }
+
+    public static ArrayList<User> getUser() {
         return userList;
-    }public static boolean updateUser(int index,User user){
+    }
+
+    public static boolean updateUser(int index, User user) {
         userList.set(index, user);
         return true;
+    }
+
+    public static void saveData() {
+
+    }
+
+    public static void loadData() {
+
     }
 }
