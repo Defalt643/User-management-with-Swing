b26744f06e23eb7abd98cdb3a5397a379e1c9a16
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-08-15 14:02:58 +0700

    add code in UserService

--- src/main/java/com/defalt/usermanagement/UserService.java
+++ src/main/java/com/defalt/usermanagement/UserService.java
@@ -1,6 +1,28 @@
 package com.defalt.usermanagement;
 import java.util.*;
 public class UserService {
-    private ArrayList<User> userList = null;
-    
+    private static ArrayList<User> userList = null;
+    static{
+        userList = new ArrayList<>();
+        userList.add(new User("admin","password"));
+        userList.add(new User("user1","password"));
+        userList.add(new User("user2","password"));
+    }public static boolean addUser(User user){
+        if(userList.add(user)){
+            return true;
+        }return false;
+    }public static boolean delUser(User user){
+        if(userList.remove(user)){
+            return true;
+        }return false;
+    }public static boolean delUser(int index){
+        if(userList.remove(index)!=null){
+            return true;
+        }return false;
+    }public static ArrayList<User> getUser(){
+        return userList;
+    }public static boolean updateUser(int index,User user){
+        userList.set(index, user);
+        return true;
+    }
 }
